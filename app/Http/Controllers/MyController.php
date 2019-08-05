8d9ab29c91c9276dca44c46e4a20b25da3ce53e2
unless003 <37129836+unless003@users.noreply.github.com>
2019-08-05 07:28:25 +0700

    Update MyController.php

--- app/Http/Controllers/MyController.php
+++ app/Http/Controllers/MyController.php
@@ -8,63 +8,9 @@ use Illuminate\Http\Response;
 class MyController extends Controller
 {
     //
-    public function hello()
-    {
-    	echo "hello // test controller ";
-    }
-    public function test($ten)
-    {
-    	echo "watashi wa ".$ten;
-   // 	return redirect()->route('MyRoute');
-    }
+ 
 
-    public function GetURL(Request $request)
-    {
-    //	return $request->path();
-    	//	return $request->url();
-    	// if ($request->isMethod('get')) {
-    	// 	echo "Phuong thuc get";
-    	// }else
-    	// echo "khong phai phuong thuc get";
-
-    	if($request->is('My*'))
-    		echo "có My";
-    	else
-    		echo "ko có My";
-    }
-    public function postForm(Request $request)
-    {
-    	echo $request->HoTen;
-    }
-
-    public function setCookie()
-    {
-
-    	$response = new Response();
-    	$response->withCookie('Koha','Laravel',1);
-    	echo "đã set Cookie";
-    	return $response;
-    }
-
-    public function getCookie(Request $request )
-    {
-    	echo "Cookie của bạn là ";
-    	return $request->Cookie('Koha');
-    }
-
-    public function postFile(Request $request){
-    	
-    }
-
-
-    public function myView()
-    {
-    	return view('test');
-    }
-    // public function test()
-    // {
-    //     return view('test');
-    // }
+   
      public function index()
     {
         return view('pages/index');
